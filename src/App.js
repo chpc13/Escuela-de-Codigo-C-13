36929322a786f5565f686dc0f5463ff53a1726f4
chpc13 <chpcodigo13@gmail.com>
2022-12-03 10:03:21 -0500

    Realizando enrutamiento

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from "react";
 import Navbar from "./Components/Navbar/Navbar.jsx";
-// import Login from "./Components/Login/Login.jsx";
+import Login from "./Components/Login/Login.jsx";
 import News from "./Components/News/News.jsx";
 import Career from "./Components/Career/Career.jsx";
 import Footer from "./Components/Footer/Footer.jsx";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <div>
       <Navbar />
-      {/* <Login/> */}
-      <News />
-      <Career />
+      <Routes>
+        <Route path="/Login" element={<Login />} />
+        <Route path="/News" element={  <News /> }/>
+        <Route path="/Career" element={  <Career/> }/>
+      </Routes>
       <Footer />
     </div>
   );
